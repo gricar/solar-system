20f2c86de47ae606889c61d6b6cdd3ef56ce6d10
gricar <gabriel.rodrigues.ricardo@outlook.com>
2022-01-25 16:23:23 -0300

    requisito 9 - ok

--- src/components/MissionCard.js
+++ src/components/MissionCard.js
@@ -2,7 +2,15 @@ import React, { Component } from 'react';
 
 class MissionCard extends Component {
   render() {
-    return ();
+    const { name, year, country, destination } = this.props;
+    return (
+      <div data-testid="mission-card">
+        <p data-testid="mission-name">{ name }</p>
+        <p data-testid="mission-year">{ year }</p>
+        <p data-testid="mission-country">{ country }</p>
+        <p data-testid="mission-destination">{ destination }</p>
+      </div>
+    );
   }
 }
 
