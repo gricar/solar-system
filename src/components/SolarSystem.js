0efe7f70544852f00bb190f205ca77bafd31f140
gricar <gabriel.rodrigues.ricardo@outlook.com>
2022-01-25 15:00:48 -0300

    requisito 2 - ok

---
+++ src/components/SolarSystem.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class SolarSystem extends Component {
+  render() {
+    return (
+      <div data-testid="solar-system">
+      </div>
+    );
+  }
+}
+
+export default SolarSystem;
