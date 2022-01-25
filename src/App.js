a2ac70ac3441b35c7174a0a96f3ace632d3e33fe
gricar <gabriel.rodrigues.ricardo@outlook.com>
2022-01-25 15:17:31 -0300

    requisito 4 - ok

--- src/App.js
+++ src/App.js
@@ -1,16 +1,13 @@
 import React from 'react';
 import Header from './components/Header';
 import SolarSystem from './components/SolarSystem';
-import Title from './components/Title';
 
 class App extends React.Component {
   render() {
-    const text = 'planetsExemplo';
     return (
       <>
         <Header />
         <SolarSystem />
-        <Title headline={ text } />
       </>
     );
   }
